451f01bf36c60ca7adce9863c16a8d9c91384709
José Lin <lin-jose@outlook.com>
2022-06-22 23:04:45 -0200

    Pulish the TaskEdit style

--- components/TaskEdit.js
+++ components/TaskEdit.js
@@ -1,5 +1,6 @@
-import { useEffect, useRef, useState } from 'react';
-import { RiCloseCircleFill } from 'react-icons/ri';
+import { useState } from 'react';
+import { RiCloseCircleFill, RiDeleteBin5Fill } from 'react-icons/ri';
+import { MdSaveAlt } from 'react-icons/md'
 import styles from '../styles/taskedit.module.css';
 
 export default function TaskEdit({ trigger, task, setTrigger, updateTask, removeTask }) {
@@ -36,34 +37,59 @@ export default function TaskEdit({ trigger, task, setTrigger, updateTask, remove
     return (trigger) ? (
         <div className={styles.popup}>
             <div className={styles.popupInner}>
-                <div>
+                <div className={styles.heading}>
                     <h2>Edit To Do</h2>
-                    <RiCloseCircleFill
+                    <button
                         onClick={handleClose}
-                    />
+                        className={styles.iconBtn}>
+                        <RiCloseCircleFill />
+                    </button>
                 </div>
-                <div>
-                    <h3>Title</h3>
+                <div className={styles.formGroup}>
+                    <div>
+                        <label className={styles.label}>Title</label>
+                    </div>
                     <input
+                        className={styles.formControl}
                         type="text"
                         name="title"
+                        placeholder="Add a title"
                         onChange={handleChange}
                         value={editInput.title}
                         autoFocus
                     />
                 </div>
-                <div>
-                    <h3>Notes</h3>
+                <div className={styles.formGroup}>
+                    <div>
+                        <label className={styles.label}>Notes</label>
+                    </div>
                     <textarea
+                        className={styles.formControl}
                         type="text"
                         name="notes"
                         onChange={handleChange}
                         value={editInput.notes}
                     />
                 </div>
-                <div>
-                    <button onClick={handleEditSubmit}>Save</button>
-                    <button onClick={() => removeTask(task.id)}>Delete To Do</button>
+                <div className={styles.fGroup3}>
+                    <div className={styles.fLeft}></div>
+                    <div className={styles.fCenter}>
+                        <button
+                            className={styles.flexBtn}
+                            onClick={handleEditSubmit}>
+                            <MdSaveAlt className={styles.iconBtn}/>&nbsp;&nbsp;Save
+                        </button>
+                    </div>
+                    <div className={styles.fRight}>
+                        <button
+                            className={styles.iconBtn}
+                            onClick={() => removeTask(task.id)}>
+                            <RiDeleteBin5Fill />
+                        </button>
+                    </div>
+                    
+                    
+                    
                 </div>
             </div>
         </div>
